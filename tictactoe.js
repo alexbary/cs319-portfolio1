cd2bd3e02b00971e46ee984060eb6f3ae3165453
sydehlin <sydehlin@iastate.edu>
2017-09-28 18:06:20 -0500

    Working on changing modes

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,17 @@
-console.log("test");
+//two modes, "Against Computer" the program will randomly select an unfilled spot
+//and place their symbol there there.
+//If we have time maybe do different modes, so easy just selects a random spot,
+//and medium sees if there is any two in a row and place it there, idk about what
+//hard would be
+//"Against User" the program will alternate symbols
+
+//computer=0 user=1, always defaults to computer mode
+var mode = 0;
+
+function computer(){
+  mode = 0;
+}
+
+function user(){
+  mode = 1;
+}
