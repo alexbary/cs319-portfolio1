6c4bf189cbc59a6e2116f9f61eb994897fe0c8a5
sydehlin <sydehlin@iastate.edu>
2017-10-13 15:08:12 -0500

    adding white pieces to board

--- chess.js
+++ chess.js
@@ -12,27 +12,8 @@ for(i = 0; i < 8; i++){
     ctx.stroke();
     var left = 0;
 
-
-    addPieces("blackRook", 10, 10); //10x10 and 500x8
-    addPieces("blackRook", 500, 8);
-
-    addPieces("blackKnight", 80, 8); // 80x8 and 430x8
-    addPieces("blackKnight", 430, 8); // 80x8 and 430x8
-
-    addPieces("blackBishop", 150, 8); //150x8 and 360x8
-    addPieces("blackBishop", 360, 8); //150x8 and 360x8
-
-    addPieces("blackQueen", 220, 8); //220x8
-    addPieces("blackKing", 290, 8); //290x8
-
-    addPieces("blackPawn", 10, 80); //10x80
-    addPieces("blackPawn", 80, 80);
-    addPieces("blackPawn", 150, 80);
-    addPieces("blackPawn", 220, 80);
-    addPieces("blackPawn", 290, 80);
-    addPieces("blackPawn", 360, 80);
-    addPieces("blackPawn", 430, 80);
-    addPieces("blackPawn", 500, 80);
+    addBlackPieces();
+    addWhitePieces();
 
     for(var a = 0; a < 8; a++) {
       for(var b = 0; b < 8; b += 2) {
@@ -43,6 +24,54 @@ for(i = 0; i < 8; i++){
       }
     }
   }
+}
+
+function addBlackPieces(){
+  addPieces("blackRook", 10, 10); //10x10 and 500x8
+  addPieces("blackRook", 500, 8);
+
+  addPieces("blackKnight", 80, 8); // 80x8 and 430x8
+  addPieces("blackKnight", 430, 8); // 80x8 and 430x8
+
+  addPieces("blackBishop", 150, 8); //150x8 and 360x8
+  addPieces("blackBishop", 360, 8); //150x8 and 360x8
+
+  addPieces("blackQueen", 220, 8); //220x8
+  addPieces("blackKing", 290, 8); //290x8
+
+  //find a way to loop through these instead of doing each one individually
+  addPieces("blackPawn", 10, 80); //10x80
+  addPieces("blackPawn", 80, 80);
+  addPieces("blackPawn", 150, 80);
+  addPieces("blackPawn", 220, 80);
+  addPieces("blackPawn", 290, 80);
+  addPieces("blackPawn", 360, 80);
+  addPieces("blackPawn", 430, 80);
+  addPieces("blackPawn", 500, 80);
+}
+
+function addWhitePieces(){
+  addPieces("whiteRook", 10, 500); //10x10 and 500x8
+  addPieces("whiteRook", 500, 500);
+
+  addPieces("whiteKnight", 80, 500); // 80x8 and 430x8
+  addPieces("whiteKnight", 430, 500); // 80x8 and 430x8
+
+  addPieces("whiteBishop", 150, 500); //150x8 and 360x8
+  addPieces("whiteBishop", 360, 500); //150x8 and 360x8
+
+  addPieces("whiteQueen", 220, 500); //220x8
+  addPieces("whiteKing", 290, 500); //290x8
+
+  //find a way to loop through these instead of doing each one individually
+  addPieces("whitePawn", 10, 430); //10x80
+  addPieces("whitePawn", 80, 430);
+  addPieces("whitePawn", 150, 430);
+  addPieces("whitePawn", 220, 430);
+  addPieces("whitePawn", 290, 430);
+  addPieces("whitePawn", 360, 430);
+  addPieces("whitePawn", 430, 430);
+  addPieces("whitePawn", 500, 430);
 }
 
 function addPieces(piece, x, y){
